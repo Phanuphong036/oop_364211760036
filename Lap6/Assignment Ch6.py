8d155663d22880ab66686cd84317e09b19875d04
phanuphong <phanuphong.s@rmutsvmail.com>
2023-02-01 15:56:46 +0700

    add hello2.py

--- Lap6/Assignment Ch6.py
+++ Lap6/Assignment Ch6.py
@@ -8,6 +8,15 @@ OOP Exercise Chapter 6
 ความรเร็วสูงสุด (max speed)
 ราคา (price)
 """
+
+"""
+
+2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes)ของคลาส
+จากนั้นแสดงข้อมูลทางหน้าจอภาพ
+
+15 นาที
+"""
+
 Vahicle_store = []
 num = int(input('Vahicle? :'))
 
@@ -18,22 +27,8 @@ for x in range(num):
     speed = input('ความเร็วสูงสุด: ')
     price = float(input('ราคา: '))
 
-    # 1
-    b = Vahicle(brand,model,color,speed,price)
-    Vahicle_store.append(b)
-    # 2
-    #book_store.append(Book(bookname,price,auther,publisher))
-
-def display_Vahicle(Vahicle):
-    print('จำนวนรถ:',len(Vahicle))
-    for x in Vahicle:
-        x.Vahicle_detail()
-
-display_Vahicle(Vahicle_store)
-"""
-
-2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes)ของคลาส
-จากนั้นแสดงข้อมูลทางหน้าจอภาพ
-
-15 นาที
-"""
+    print("ยี่ห้อรถ : ",brand)
+    print("รุ่นรถ : ",model)
+    print("สีรถ : ",color)
+    print("ความรเร็วสูงสุด : ",speed)
+    print("ราคา : ",price)
